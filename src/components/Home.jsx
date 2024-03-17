0b5cabaffb59bf6671e546d833bc24b8be36c2ec
Tapishagrawal <tapishg4@gmail.com>
2024-03-17 17:26:48 +0530

    add button to cart

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Card from './Card'
-
 export default function Home() {
+  const dealAlrt = () =>{
+    alert("deal clcik")
+  }
+  const detailAlrt = () =>{
+    alert("detail clcik")
+  }
   return (
     <div className='flex justify-center items-center h-[96vh]'>
       <div className='flex gap-4 justify-around'>
-        <Card heading={"save deals in the system"} btnTitle={"Add to Deals"}/>
-        <Card heading={"Get deals from the system"} btnTitle={"Check Details"}/>
+        <Card handleClick={dealAlrt} heading={"save deals in the system"} btnTitle={"Add to Deals"} />
+        <Card handleClick={detailAlrt} heading={"Get deals from the system"} btnTitle={"Check Details"} />
       </div>
     </div>
   )
